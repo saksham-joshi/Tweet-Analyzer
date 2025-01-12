306037febed786bc2cd883447aeb22047e64487d
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-12 15:06:43 +0530

    Created: Authentication-based variables

---
+++ Tweet_Extractor/base/auth_keys.py
@@ -0,0 +1,14 @@
+
+from typing import Final
+
+X_JSON_KEY_OF_API_KEY : Final = "api-key"
+X_JSON_KEY_OF_API_SECRET_KEY : Final = "api-secret-key"
+X_JSON_KEY_OF_ACCESS_TOKEN : Final = "access-token"
+X_JSON_KEY_OF_ACCESS_SECRET_TOKEN : Final = "access-secret-token"
+
+X_API_KEY : str = ""
+X_API_SECRET_KEY : str = ""
+X_ACCESS_TOKEN : str = ""
+X_ACCESS_SECRET_TOKEN : str = ""
+
+
