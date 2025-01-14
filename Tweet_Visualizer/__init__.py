707e2c619ff6cc7abfc76ab45777e0be2a1f40df
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-14 22:20:13 +0530

    Minor spaces

--- Tweet_Visualizer/__init__.py
+++ Tweet_Visualizer/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template , Response , jsonify , url_for
 from flask_cors import CORS
+from flask_socketio import SocketIO
 from Tweet_Visualizer.graph import *
 
 TWEET_APP__ = Flask(__name__)
+TWEET_SOCKET__ = SocketIO(TWEET_APP__)
 
 CORS(TWEET_APP__)
 
 TOTAL_WEBSITE_VISITORS__ : int = 0
 TOTAL_DATA_FETCHES__ : int = 0
+
+
 
 @TWEET_APP__.route('/')
 def getIndexHtml() : 
@@ -15,8 +19,11 @@ def getIndexHtml() :
     TOTAL_WEBSITE_VISITORS__ += 1
     return render_template("index.html", total_website_visitors= TOTAL_WEBSITE_VISITORS__, total_data_fetches= TOTAL_DATA_FETCHES__, graph_figure = open("Tweet_Visualizer/static/sorry_image.png"))
 
+
+# function to display graphs of particulars companies
 @TWEET_APP__.route('/company/<__company_name>')
 def getGraphOfCompany(__company_name : str) : 
+
     graph_to_transmit = GRAPH_OF_ALL__.get(__company_name, None)
 
     if graph_to_transmit == None : Response()
