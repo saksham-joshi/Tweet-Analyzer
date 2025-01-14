11a8878098164a3b37eb9d1ae38e0c76eae70367
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-14 19:20:01 +0530

    Modified: summing up all parts of Tweet-Analyzer

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from Tweet_Extractor import *
-#from Tweet_Sentiment import analyzer
-#from Tweet_Visualizer import *
+from Tweet_Sentiment import analyzer
+from Tweet_Visualizer import *
 
 
 if __name__ == "__main__" :
-    try :
-         setupTweetExtractor()
+    try :    
+        setupTweetExtractor()
+        TWEET_APP__.run(debug=True, host="0.0.0.0", port= 5000)
+    
     except XException_AuthenticationKeysNotFound as __excep:
         print(__excep)
-
-
