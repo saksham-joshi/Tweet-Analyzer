ef41b1b56789cb537c2c9ce0ead35a6beeb5a9f3
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-17 10:12:10 +0530

    Caching implemented | Added: methods for more features

--- Tweet_Visualizer/graph/generator.py
+++ Tweet_Visualizer/graph/generator.py
@@ -1,5 +1,6 @@
 from plotly.express import bar
 from plotly.io import to_json , to_html
+from flask import jsonify
 
 class XGraph :
 
@@ -9,7 +10,10 @@ class XGraph :
         this._negative_tweets : int = __negative_tweet_count
         this._neutral_tweets : int = __neutral_tweet_count
         this._bar_graph_json = None
+        this._jsonified_graph_response = None
+        this._jsonified_data_response = None
     
+
     def getBarGraph(this) :
         if this._bar_graph_json is None :
             this._bar_graph_json = to_json( bar ( {
@@ -19,7 +23,38 @@ class XGraph :
                     color_discrete_sequence= ["#20A6DF"]
                 ) )
         return this._bar_graph_json
+    
 
+    def getTweetsCount(this) -> dict[str , int] :
+        return {
+            "positive-tweets-count" : this._positive_tweets,
+            "negative-tweets-count" : this._negative_tweets,
+            "neutral-tweets-count" : this._neutral_tweets
+        }
+    
+
+    def getJsonifiedGraphResponse(this) :
+        
+        if this._jsonified_graph_response is None :
+            this._jsonified_graph_response = jsonify( {
+                "response_status" : 200,
+                "graph" : this.getBarGraph()
+            } )
+
+        return this._jsonified_graph_response , 200
+    
+
+    def getJsonifiedDataResponse(this) :
+        if this._jsonified_data_response is None :
+
+            this._jsonified_data_response = jsonify({
+
+                "company-name" : this._brand,
+                "response-status" : 200,
+                "counts" : this.getTweetsCount()
+            }) , 200
+
+        return this._jsonified_data_response
 
 GRAPH_DICT_OF_ALL__ : dict[str : XGraph] = {
 
@@ -50,17 +85,27 @@ def generateTogetherGraph() :
     negative_counts = []
 
     for brand, graph_obj in GRAPH_DICT_OF_ALL__.items():
+
         brands.append(brand)
+
         positive_counts.append(graph_obj._positive_tweets)
+
         neutral_counts.append(graph_obj._neutral_tweets)
+
         negative_counts.append(graph_obj._negative_tweets)
 
     global GRAPH_TOGETHER_OF_ALL_BRANDS__
+
     GRAPH_TOGETHER_OF_ALL_BRANDS__ = to_html( bar ( {
+
         "Brands": brands * 3,
+        
         "Sentiments": ["Positive"] * len(brands) + ["Neutral"] * len(brands) + ["Negative"] * len(brands),
+        
         "Count": positive_counts + neutral_counts + negative_counts
+    
     }, x="Brands", y="Count", color="Sentiments", barmode="group", title= "Analysis for all brands in last 7 days."
+     
      , color_discrete_sequence= ["#00cc96" , "#636efa" , "#ef553b"] ), full_html= False)
 
 generateTogetherGraph()
