b908ae0ca092249222d02a74ecc1d21788a512bf
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-20 00:00:50 +0530

    Fixed: if __name__ == '__main__' not running using flask cmd

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from Tweet_Extractor import *
-from Tweet_Sentiment import analyzer
+from Tweet_Sentiment import *
 from Tweet_Visualizer import *
 
 
-if __name__ == "__main__" :
-    try :    
+if __name__ == "__main__" or __name__ == "app" :
+    try :
+        print("\n Launching the APP!")
         setupTweetExtractor()
+        #setupTweetVisualizer()
         TWEET_APP__.run(debug=True, host="0.0.0.0", port= 5000)
     
     except XException_AuthenticationKeysNotFound as __excep:
