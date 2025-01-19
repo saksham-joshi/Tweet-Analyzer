8fb706126d6d9d5f15c6076abc340a478038603c
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-20 00:03:25 +0530

    Minor modification

--- Tweet_Visualizer/base.py
+++ Tweet_Visualizer/base.py
@@ -7,13 +7,18 @@ TWEET_APP__ = Flask(__name__)
 
 CORS(TWEET_APP__)
 
+CONTEXT_APP = TWEET_APP__.app_context()
+
 TOTAL_WEBSITE_VISITORS__ : int = 0
 TOTAL_DATA_FETCHES__ : int = 0
 
 URL_TO_COMPANY_DATA_NOT_AVAILABLE_IMAGE : Final = "https://cdn.jsdelivr.net/gh/saksham-joshi/Tweet-Analyzer@main/Tweet_Visualizer/static/img/sorry_image.png"
-
-
-def incrementTotalDataFetches() : 
-   global TOTAL_DATA_FETCHES__
-   TOTAL_DATA_FETCHES__ += 1
     
+# store the rendered template of main html page
+INDEX_TEMPLATE__ = None
+
+# stores the dictionary to response for 404 error
+RESPONSE_TO_404 = None
+
+# if we do not provide data for a certain company then this response is used
+COMPANY_DATA_NOT_PROVIDED_BY_US = None
