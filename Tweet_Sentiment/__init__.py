d4a4c9d463a8de1ed72b790c847db6fcd19135f2
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-01-20 00:02:54 +0530

    Defined all the required methods

--- Tweet_Sentiment/__init__.py
+++ Tweet_Sentiment/__init__.py
@@ -1,1 +1,7 @@
 from textblob import TextBlob
+
+def getTypeOfSentiment(__polarity_score : float) -> str :
+    return "Positive" if __polarity_score > 0 else "Neutral" if __polarity_score == 0 else "Negative"
+
+def getSentiment(__tweet : str) -> str :
+    return getTypeOfSentiment(TextBlob(__tweet).polarity)
