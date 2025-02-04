471e12359686b0604ff16ae9e16690d4a9a2339b
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-02-04 15:41:18 +0530

    Beautified app.py

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from Tweet_Visualizer import *
 
 
 if __name__ == "__main__" or __name__ == "app" :
+
     try :
         print("\n Launching the APP!")
+
         setupTweetExtractor()
+
         TWEET_APP__.run(debug=True, host="0.0.0.0", port= 5000)
     
     except XException_AuthenticationKeysNotFound as __excep:
+        
         print(__excep)
