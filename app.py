e79f225423781213fda9bee37698594bdaf9d70e
SAKSHAM JOSHI <social.sakshamjoshi@gmail.com>
2025-02-04 14:19:19 +0530

    Removed: unnecessary comment

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ if __name__ == "__main__" or __name__ == "app" :
     try :
         print("\n Launching the APP!")
         setupTweetExtractor()
-        #setupTweetVisualizer()
         TWEET_APP__.run(debug=True, host="0.0.0.0", port= 5000)
     
     except XException_AuthenticationKeysNotFound as __excep:
